0d0b979c6fbee70391063d64b358f1fa0c0927e8
Graham Lynch <grahamlynch10@hotmail.com>
2020-04-15 12:10:32 +0100

    First Commit

---
+++ plane_detect.py
@@ -0,0 +1,73 @@
+import cv2
+import numpy as np
+import json
+import io
+import re
+import imutils 
+from firebase import firebase
+from matplotlib import pyplot as plt
+
+userId = "htui1UTIEaTvs1Ql51S8GuNwG6C3"
+
+firebase = firebase.FirebaseApplication('https://smartport-68b3c.firebaseio.com/',None)
+
+cap = cv2.VideoCapture(0)
+
+cv2.namedWindow("Frame")
+points = []
+while True:
+    _, frame = cap.read()
+    hsv_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    ## define colour detection
+    # Define upper and lower red ranges
+    lower_green = np.array([22, 94, 114])
+    upper_green = np.array([179, 255, 255])
+
+    # Define upper and lower blue ranges
+    lower_blue = np.array([91, 156, 32])
+    upper_blue = np.array([111, 176, 112])
+
+
+    bluemask = cv2.inRange(hsv_frame, lower_blue, upper_blue)
+    blue = cv2.bitwise_and(frame, frame, mask=bluemask)
+    blue = cv2.cvtColor(blue, cv2.COLOR_HSV2BGR)
+    blue = cv2.cvtColor(blue, cv2.COLOR_BGR2GRAY)
+    bluecount = cv2.countNonZero(blue)
+
+    greenmask = cv2.inRange(hsv_frame, lower_green, upper_green)
+    green = cv2.bitwise_and(frame, frame, mask=greenmask)
+    green = cv2.cvtColor(green, cv2.COLOR_HSV2BGR)
+    green = cv2.cvtColor(green, cv2.COLOR_BGR2GRAY)
+    greencount = cv2.countNonZero(green)
+
+
+    plane_cascade = cv2.CascadeClassifier('planecascade.xml')
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    #cv2.rectangle(frame, (x1, y1), (x2, y2), (255, 0, 0), 2)
+   
+    #print result
+    planes = plane_cascade.detectMultiScale(gray, 1.1, 25)
+    for (x,y,w,h) in planes:
+	cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
+	print "Green"+ str(greencount)
+	print "Blue"+ str(bluecount)
+	if (greencount < 1000) and planes.all():
+		print "Ryanair"
+		firebase.put(userId,'flight','Ryanair')
+	else:
+			pass
+	if (greencount > 1000) and planes.all():
+		print "Aer Lingus"
+		firebase.put(userId,'flight','Aer Lingus')
+    cv2.imshow("frame", frame)
+	#cv2.imshow("Bluemask", bluemask)
+	#cv2.imshow("Greenmask", greenmask)
+
+    key = cv2.waitKey(1)
+    if key == 27:
+        break
+    elif key == ord("d"):
+        circles = []
+cap.release()
+cv2.destroyAllWindows()
